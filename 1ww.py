66d470da4ee14c069c1bf8f8d6df52f3da195999
David <DaveJ61@users.noreply.github.com>
2015-04-21 20:17:02 +0100

    Delete 1ww.py

--- 1ww.py
+++
@@ -1,8 +0,0 @@
-# Initial test using pyonewire (https://code.google.com/p/pyonewire)
-# Still need to figure out linix permissions for the DS2490 (USB)
-# but does work using sudo
-
-from pyonewire.master import ds2490
->>> master = ds2490.DS2490Master()
->>> for ib in master.Search(master.SEARCH_NORMAL):
-...   print hex(ib)
